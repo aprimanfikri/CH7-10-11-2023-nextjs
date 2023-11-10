1b4700414f01fb48ba60117556b6aba3a49a4457
aprimanfikri <aprmnfkr@gmail.com>
2023-11-10 21:49:21 +0700

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <>
+      <div>Hallo FSW 2</div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus omnis ex
+        magni expedita error, quis ad cum voluptatum totam id, quisquam facere
+        ratione nam eos dignissimos consequuntur eveniet iure. Iure.
+      </p>
+    </>
+  );
+};
+
+export default About;
